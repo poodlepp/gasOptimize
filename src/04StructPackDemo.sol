65bc4c6336b7254b112dd7bb849a39e60335d35c
poodlepp <1943543964@qq.com>
2024-06-19 20:20:42 +0800

    04-05

---
+++ src/04StructPackDemo.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+// The order ot the variables affects the slot allocation.
+
+contract StructPackDemo {
+    struct unpackedStruct {
+        uint64 time; // Takes one slot - although it  only uses 64 bits (8 bytes) out of 256 bits (32 bytes).
+        uint256 money; // This will take a new slot because it is a complete 256 bits (32 bytes) value and thus cannot be packed with the previous value.
+        address person; // An address occupies only 160 bits (20 bytes).
+    }
+
+    // Starts at slot 0
+    unpackedStruct details1 =
+        unpackedStruct(53_000, 21_000, address(0xdeadbeef));
+
+    function unpack1() external view returns (unpackedStruct memory) {
+        return details1;
+    }
+
+    struct packedStruct {
+        uint64 time; // In this case, both `time` (64 bits) and `person` (160 bits) are packed in the same slot since they can both fit into 256 bits (32 bytes)
+        address person; // Same slot as `time`. Together they occupy 224 bits (28 bytes) out of 256 bits (32 bytes).
+        uint256 money; // This will take a new slot because it is a complete 256 bits (32 bytes) value and thus cannot be packed with the previous value.
+    }
+
+    // Starts at slot 0
+    packedStruct details2 = packedStruct(53_000, address(0xdeadbeef), 21_000);
+
+    function unpack2() external view returns (packedStruct memory) {
+        return details2;
+    }
+}
---
+++ src/05ShortStringDemo.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+/**
+ * short string which less than 32 bytes  just capture 1 slot;
+ * so limit the string in 32 bytes is meaningful.
+ *
+ * short string   0x  length*2  ---   data
+ * long string   slot1  just length*2+1     slot2 data ...
+ */
